6285dbba803711d1c3f6bd49a3b6d87d57a79920
Daniel Bovington <d.bovington@gmail.com>
2019-03-24 14:30:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,5 @@
 # Write your code here.
 katz_deli = []
 def line(katz_deli)
-  if katz_deli.index == 0
-    puts "The line is currently empty."
-  else
-    puts "There are people inline"
-    puts katz_deli
-  end
+  puts "The line is currently empty."
 end
